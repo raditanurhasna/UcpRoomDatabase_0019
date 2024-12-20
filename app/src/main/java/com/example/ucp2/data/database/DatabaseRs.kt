a27f4d27b0a70b8d6af86b25c110a25bc5d5e641
raditanurhasna <raditanurhasna1410@gmail.com>
2024-12-20 11:14:15 +0700

    Membuat DatabaseRs dengan konfigurasi Room untuk Dokter dan Jadwal.

---
+++ app/src/main/java/com/example/ucp2/data/database/DatabaseRs.kt
@@ -0,0 +1,33 @@
+package com.example.ucp2.data.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.ucp2.data.DAO.DokterDao
+import com.example.ucp2.data.DAO.JadwalDao
+import com.example.ucp2.data.entity.Dokter
+
+@Database(entities = [Dokter::class], [JadwalDao::class], version = 1, exportSchema = false)
+abstract class DatabaseRs : RoomDatabase() {
+
+    abstract fun DokterDao(): DokterDao
+    abstract fun JadwalDao(): JadwalDao
+
+    companion object {
+        @Volatile
+        private var Instance: DatabaseRs? = null
+
+        fun getDatabase(context: Context): DatabaseRs {
+            return (Instance ?: synchronized(this) {
+                Room.databaseBuilder(
+                    context.applicationContext,
+                    DatabaseRs::class.java,
+                    "DatabaseRS"
+                )
+                    .build().also { Instance = it }
+            })
+        }
+    }
+}
+
