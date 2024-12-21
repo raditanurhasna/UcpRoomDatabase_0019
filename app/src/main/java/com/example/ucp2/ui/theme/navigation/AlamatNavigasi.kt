cc716c6dd428f9ad4f6ecffebdb27d5f2ec2a28a
raditanurhasna <raditanurhasna1410@gmail.com>
2024-12-21 19:51:35 +0700

    Menambahkan DestinasiDetail dengan route "detail" dan routesWithArg

--- app/src/main/java/com/example/ucp2/ui/theme/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/ucp2/ui/theme/navigation/AlamatNavigasi.kt
@@ -8,3 +8,14 @@ object DestinasiHome : AlamatNavigasi {
     override val route = "home"
 }
 
+object DestinasiDetail : AlamatNavigasi {
+    override val  route = "detail"
+    const val ID = "Id"
+    val routesWithArg = "$route/{$ID}"
+}
+
+object DestinasiUpdate : AlamatNavigasi {
+    override val route = "update"
+    const val ID = "id"
+    val routesWithArg  = "$route/{$ID}"
+}
