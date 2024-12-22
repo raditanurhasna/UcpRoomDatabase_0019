42a471894e1d3c1706fb456c0a4ddebfbf2dda15
raditanurhasna <raditanurhasna1410@gmail.com>
2024-12-22 16:14:13 +0700

    Menambahkan Tampilan home dokter

---
+++ app/src/main/java/com/example/ucp2/ui/theme/view/dokter/HomeDokterView.kt
@@ -0,0 +1,161 @@
+package com.example.ucp2.ui.theme.view.dokter
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2.data.entity.Dokter
+import com.example.ucp2.ui.theme.customewidget.TopAppBar
+import com.example.ucp2.ui.theme.viewmodel.HomeDokterViewModel
+import com.example.ucp2.ui.theme.viewmodel.HomeUiState
+import com.example.ucp2.ui.theme.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.launch
+
+@Composable
+fun HomeDokterView(
+    viewModel: HomeDokterViewModel = viewModel(factory = PenyediaViewModel.Factory ),
+    onAddDokter: () -> Unit = { },
+    onDetailClick: (String) -> Unit = { },
+    modifier: Modifier = Modifier
+){
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                judul = "Daftar Dokter",
+                showBackButton = false,
+                onBack = { },
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onAddDokter,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Tambah Dokter",
+                )
+            }
+        }
+    ) { innerPadding ->
+        val homeUiState by viewModel.homeUIState.collectAsState()
+
+        BodyHomeDokterView(
+            homeUiState = homeUiState,
+            onClick = {
+                onDetailClick(it)
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+
+    }
+}
+
+@Composable
+fun BodyHomeDokterView(
+    homeUiState: HomeUiState,
+    onClick: (String) -> Unit = { },
+    modifier: Modifier = Modifier
+){
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+    when {
+        homeUiState.isLoading -> {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+            ){
+                CircularProgressIndicator()
+            }
+        }
+        homeUiState.isError -> {
+            LaunchedEffect(homeUiState.errorMessage) {
+                homeUiState.errorMessage?.let { message ->
+                    coroutineScope.launch {
+                        snackbarHostState.showSnackbar(message)
+                    }
+                }
+            }
+        }
+        homeUiState.listDokter.isEmpty() -> {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                Text(
+                    text = "Tidak ada data mahasiswa.",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+
+        else -> {
+            ListDokter(
+                listDokter = homeUiState.listDokter,
+                onClick = {
+                    onClick(it)
+                    println(
+                        it
+                    )
+                },
+                modifier = modifier
+            )
+        }
+    }
+}
+
+@Composable
+fun ListDokter(
+    listDokter: List<Dokter>,
+    modifier: Modifier = Modifier,
+    onClick: (String) -> Unit = {}
+){
+    LazyColumn (
+        modifier = modifier
+    ){
+        items(
+            items= listDokter,
+            itemContent = { dokter ->
+                CardDokter(
+                    dokter = dokter,
+                    onClick = { onClick(dokter.id) }
+                )
+            }
+        )
+    }
+}
+
+
