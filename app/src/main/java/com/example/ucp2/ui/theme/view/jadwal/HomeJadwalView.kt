b85e6fafb4beb5bb2acd463cc874af4c7cedfe5b
raditanurhasna <raditanurhasna1410@gmail.com>
2024-12-23 10:23:39 +0700

    Menambahkan HomeJadwalView

---
+++ app/src/main/java/com/example/ucp2/ui/theme/view/jadwal/HomeJadwalView.kt
@@ -0,0 +1,243 @@
+package com.example.roomlocaldb.ui.theme.view.mahasiswa
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2.data.entity.Jadwal
+import com.example.ucp2.ui.theme.customewidget.TopAppBar
+import com.example.ucp2.ui.theme.viewmodel.HomeJadwalUiState
+import com.example.ucp2.ui.theme.viewmodel.HomeJadwalViewModel
+import com.example.ucp2.ui.theme.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.launch
+
+@Composable
+fun HomeJadwalView(
+    viewModel: HomeJadwalViewModel = viewModel(factory = PenyediaViewModel.Factory ),
+    onAddJadwal: () -> Unit = { },
+    onDetailClick: (String) -> Unit = { },
+    modifier: Modifier = Modifier
+){
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                judul = "Daftar Pasien",
+                showBackButton = false,
+                onBack = { },
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onAddJadwal,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Tambah Pasien",
+                )
+            }
+        }
+    ) { innerPadding ->
+        val homeJadwalUiState by viewModel.homeJadwalUIState.collectAsState()
+
+        BodyHomeJadwalView(
+            homeJadwalUiState = homeJadwalUiState,
+            onClick = {
+                onDetailClick(it)
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+
+    }
+}
+
+@Composable
+fun BodyHomeJadwalView(
+    homeJadwalUiState: HomeJadwalUiState,
+    onClick: (String) -> Unit = { },
+    modifier: Modifier = Modifier
+){
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+    when {
+        homeJadwalUiState.isLoading -> {
+
+            Box(
+                modifier = Modifier.fillMaxSize(),
+            ){
+                CircularProgressIndicator()
+            }
+        }
+        homeJadwalUiState.isError -> {
+
+            LaunchedEffect(homeJadwalUiState.errorMessage) {
+                homeJadwalUiState.errorMessage?.let { message ->
+                    coroutineScope.launch {
+                        snackbarHostState.showSnackbar(message)
+                    }
+                }
+            }
+        }
+        homeJadwalUiState.listJadwal.isEmpty() -> {
+
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                Text(
+                    text = "Tidak ada data pasien.",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+
+        else -> {
+
+            ListJadwal(
+                listJadwal = homeJadwalUiState.listJadwal,
+                onClick = {
+                    onClick(it)
+                    println(
+                        it
+                    )
+                },
+                modifier = modifier
+            )
+        }
+    }
+}
+
+@Composable
+fun ListJadwal(
+    listJadwal: List<Jadwal>,
+    modifier: Modifier = Modifier,
+    onClick: (String) -> Unit = {}
+){
+    LazyColumn (
+        modifier = modifier
+    ){
+        items(
+            items = listJadwal,
+            itemContent = { jadwal ->
+                CardJadwal(
+                    jadwal = jadwal,
+                    onClick = { onClick(jadwal.id) }
+                )
+            }
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CardJadwal(
+    jadwal: Jadwal,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = { }
+){
+    Card (
+        onClick = onClick,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ){
+        Column (
+            modifier = Modifier.padding(8.dp)
+        ){
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Icon(imageVector = Icons.Filled.Person, contentDescription = "")
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = jadwal.id,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+            }
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Icon(imageVector = Icons.Filled.DateRange, contentDescription = "" )
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = jadwal.namaDokter,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp
+                )
+            }
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Icon(imageVector = Icons.Filled.Home, contentDescription = "")
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = jadwal.namaPasien,
+                    fontWeight = FontWeight.Bold,
+                )
+            }
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Icon(imageVector = Icons.Filled.Home, contentDescription = "")
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = jadwal.tanggalKonsultasi,
+                    fontWeight = FontWeight.Bold,
+                )
+            }
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Icon(imageVector = Icons.Filled.Home, contentDescription = "")
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = jadwal.status,
+                    fontWeight = FontWeight.Bold,
+                )
+            }
+        }
+    }
+}
+
+
